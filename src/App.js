672464771f865080f53361a6174a82dbecd312f7
Digilife <digilife@Digilifes-MacBook-Pro.local>
2021-02-19 21:34:34 +0700

    Props Handle

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import BTRenderMap from "./RenderWithMap/BTRenderMap";
 import DemoProps from "./Props/DemoProps";
+import BaiTapLayout from "./components/BaiTapLayout/BaiTapLayout";
+import BTProducList from "./components/BaiTapLayout/BTProducList";
+import DemoQLSP from "./Props/BTPhone/DemoQLSP";
 
 function App() {
   return (
     <div className="App">
+      {/* <BaiTapLayout/> */}
       {/* <RenderWithMap /> */}
       {/* <BTRenderMap /> */}
-      <DemoProps />
+      {/* <DemoProps /> */}
+      <DemoQLSP/>
     </div>
   );
 }
